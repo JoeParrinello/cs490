57d230085608680d66edf147f71d3b271fa1a199
Sadiq Amini <samini91@gmail.com>
2015-07-11 15:09:57 -0400

    Finished Alpha for middleware
    Should be plug and play for both front and backend just need to change the url.

--- middleware/njitlogin.php
+++ middleware/njitlogin.php
@@ -1,16 +1,22 @@
 
 <?php
-$user=$_POST["user"];
-$pass=$_POST["pass"];
+require_once 'sendTo.php';
+  //change to actual database
+$database_url="https://web.njit.edu/~sra27/test.php";
+
 
 if($_SERVER["CONTENT_TYPE"]=="application/json"){
+  $variable=json_decode(file_get_contents('php://input'), true);
+  $user=$variable["user"];
+  $pass= $variable["pass"];
 
-$variable=json_decode(file_get_contents('php://input'), true);
-$user=$variable["user"];
-$pass= $variable["pass"];
+}
+else{
+  $user=$_POST["user"];
+  $pass=$_POST["pass"];
 }
 
-
+$hashpass = hash_pbkdf2("sha256", $pass, $salt, 1000, 20);
 
 function njitlogin( $user,$pass ){
   // user=UCID&pass=pass&uuid=0xACA021
@@ -33,20 +39,29 @@ function njitlogin( $user,$pass ){
 
 	// Return validation bool
 
-
-
 	   if (strpos($result, "loginok.html") !== false)
 		  {
-		    return "njittrue";
+		    return "njit_true";
 
 		  }
 		else{
-		  return "njitfalse";}
-	//	return strpos($result, "loginok.html") !== false;
+		  return "njit_false";}
 	}
 
 
-	$a=njitlogin($user,$pass);
-        echo $a;
+$njit_bool=njitlogin($user,$pass);
+$database_bool=sendTo($database_url,array("user"=>$user,
+			   "hashpass"=>$hashpass
+			   )
+       );
+
+$njit_database_bool=json_encode(array(
+				     "njit_login"=>$njit_bool,
+				     "database_login"=>$database_bool
+				     )
+			       );
+
+
+echo $njit_database_bool;
 
 ?>
