32d41a44a277d691194db03add0c01a4d5204e42
Sadiq Amini <samini91@gmail.com>
2015-07-11 14:50:56 -0400

    Should have been returning instead of echoing
    To include use 
    require_once ' /*path of file*/ '

--- middleware/sendTo.php
+++ middleware/sendTo.php
@@ -6,12 +6,24 @@ curl_setopt($ch, CURLOPT_URL,$url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-//curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_HEADER, false);
 $output = curl_exec($ch);
 curl_close($ch);
-echo $output;
+return $output;
 }
 
+//Send to with no return data will return 1 if successful
+function sendTo_NDT($url,$postdata){
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,false);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$output = curl_exec($ch);
+curl_close($ch);
+return $output;
+}
 
 
 ?>
