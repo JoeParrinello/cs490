3bad120a8ff395779b3fba10c773a6fe4a45a1fa
Sadiq Amini <samini91@gmail.com>
2015-07-14 21:39:37 -0400

    added curlGet

--- middleware/sendTo.php
+++ middleware/sendTo.php
@@ -25,5 +25,18 @@ curl_close($ch);
 return $output;
 }
 
+function curlGet($url){
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+//curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$output = curl_exec($ch);
+curl_close($ch);
+return $output;
+}
+
+
 
 ?>
