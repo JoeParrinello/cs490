44de69f373371602ffb46adadac5447c111a470c
Sadiq Amini <samini91@gmail.com>
2015-07-14 21:38:21 -0400

    Works with both front and backend for both logins

--- middleware/njitlogin.php
+++ middleware/njitlogin.php
@@ -1,16 +1,22 @@
 
 <?php
-$user=$_POST["user"];
-$pass=$_POST["pass"];
+require_once 'sendTo.php';
+  //change to actual database
+//$database_url="https://web.njit.edu/~sra27/test.php";
+$database_url="https://web.njit.edu/~jap64/backend/user.php";
 
 if($_SERVER["CONTENT_TYPE"]=="application/json"){
+  $variable=json_decode(file_get_contents('php://input'), true);
+  $user=$variable["user"];
+  $pass= $variable["pass"];
 
-$variable=json_decode(file_get_contents('php://input'), true);
-$user=$variable["user"];
-$pass= $variable["pass"];
 }
-
-
+else{
+  $user=$_POST["user"];
+  $pass=$_POST["pass"];
+}
+$salt="salt";
+$hashpass = hash_pbkdf2("sha256", $pass, $salt, 1000, 20);
 
 function njitlogin( $user,$pass ){
   // user=UCID&pass=pass&uuid=0xACA021
@@ -33,20 +39,47 @@ function njitlogin( $user,$pass ){
 
 	// Return validation bool
 
-
-
 	   if (strpos($result, "loginok.html") !== false)
 		  {
-		    return "njittrue";
+		    return "njit_true";
 
 		  }
 		else{
-		  return "njitfalse";}
-	//	return strpos($result, "loginok.html") !== false;
+		  return "njit_false";}
 	}
 
 
-	$a=njitlogin($user,$pass);
-        echo $a;
+$njit_bool=njitlogin($user,$pass);
+
+
+
+//$database_bool=sendTo($database_url,array("user"=>$user,
+//			   "hashpass"=>$hashpass
+//			   )
+//     );
+
+$database_url=$database_url."?username=".$user;
+
+$database_user_info=curlGet($database_url);
+
+$database_user_info=json_decode($database_user_info, true);
+
+if($database_user_info["username"] == $user && $database_user_info["password"]==$hashpass){
+  $database_bool="database_true";
+
+}
+else{
+  $database_bool="database_false";
+
+}
+
+$njit_database_bool=json_encode(array(
+				     "njit_login"=>$njit_bool,
+				     "database_login"=>$database_bool,
+				     "role"=>$database_user_info["role"]
+				      )
+			       );
+
+echo $njit_database_bool;
 
 ?>
