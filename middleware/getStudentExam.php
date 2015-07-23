32fbae741db7013a8e65e8986309e0cc6f35b52c
Sadiq Amini <samini91@gmail.com>
2015-07-23 01:05:43 -0400

    needs userId and examId
    returns the that particular exam

---
+++ middleware/getStudentExam.php
@@ -0,0 +1,29 @@
+<?php
+require_once "sendTo.php";
+
+if(isJson(file_get_contents('php://input'))){
+  $Id=json_decode(file_get_contents('php://input'), true);
+}
+else{
+  $Id=$_POST;
+}
+
+//echo $Id["userId"];
+//https://web.njit.edu/~jap64/backend/examanswer.php?userId=7
+
+$getUrl="https://web.njit.edu/~jap64/backend/examanswer.php";
+
+
+$getUrl=$getUrl."?userId=".$Id["userId"];
+
+if(!isset($Id["examId"])){
+echo curlGet($getUrl);
+}
+
+else{
+
+  $getUrl=$getUrl."&examId=".$Id["examId"];
+  echo curlGet($getUrl);
+}
+
+?>
