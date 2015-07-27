6df9db65213413da6eed4495cd4f7c05a3ec335f
Sadiq Amini <samini91@gmail.com>
2015-07-27 18:35:59 -0400

    Update getAllExams.php

--- middleware/getAllExams.php
+++ middleware/getAllExams.php
@@ -1,21 +1,10 @@
 <?php
-  //sends back all of the name
+
 require_once "sendTo.php";
 $getExam_url="https://web.njit.edu/~jap64/backend/exam.php";
 
 $exam = curlGet($getExam_url);
 echo $exam;
-/*
-$exam = json_decode($exam,true);
 
-foreach($exam as $examidname){
-  $examidname=json_decode($examidname,true);
-  $AllName[$examidname["Exam_Id"]] = $examidname["name"];
-
-
-}
-echo $AllName;*/
-
-//echo $exam["name"];
 
 ?>
