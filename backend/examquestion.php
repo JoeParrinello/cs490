3d071c41a0481583c3290dc04caf5e875922aa62
Joe Parrinello <jap64@njit.edu>
2015-07-30 17:00:05 -0400

    Difficulty added

--- backend/examquestion.php
+++ backend/examquestion.php
@@ -7,8 +7,21 @@ function getExamQuestion(){
 	global $conn;
 	global $_GET;
 	header('Content-Type: application/json');
-	if (isset($_GET["examId"])) {
+	if (isset($_GET["examId"]) && isset($_GET["questionId"])) {
         $comma = 0;
+		if($rows = $conn->query("SELECT * FROM ExamQuestions WHERE examId=".$_GET["examId"]." AND questionId=".$_GET["questionId"].";")) {
+			echo "[";
+			while ($row = $rows->fetch_assoc()) {
+				if ($comma > 0) {
+					echo ",";
+				}
+				$comma++;
+				echo json_encode($row);
+			}
+			echo "]";
+		}
+    } elseif (isset($_GET["examId"])) {
+        	$comma = 0;
 		if($rows = $conn->query("SELECT * FROM ExamQuestions WHERE examId=".$_GET["examId"].";")) {
 			echo "[";
 			while ($row = $rows->fetch_assoc()) {
@@ -41,8 +54,8 @@ function makeExamQuestion(){
 	global $_POST;
 	header('Content-Type: application/json');
 	if (isset($_POST["examId"]) && isset($_POST["questionId"])) {
-        if($conn->query("INSERT INTO ExamQuestions (examId, questionId) VALUES (".$_POST['examId'].", ".$_POST['questionId'].")")===TRUE){
-            $row = $conn->query("SELECT * FROM ExamQuestions WHERE examId=".$_POST['examId']." AND questionId=".$_POST["questionId"].";");
+        if($conn->query("INSERT INTO ExamQuestions (examId, questionId, points) VALUES (".$_POST['examId'].", ".$_POST['questionId'].", ".$_POST['points'].")")===TRUE){
+            $row = $conn->query("SELECT * FROM ExamQuestions WHERE examId=".$_POST['examId']." AND questionId=".$_POST["questionId"]." AND points=".$_POST["points"].";");
             echo json_encode($row->fetch_assoc());
 		} else {
 			echo '{"err":"Failure Inserting Value!"}';
