3da6983ef0f20b0574d354a6046ffc0dd84dee89
Sadiq Amini <samini91@users.noreply.github.com>
2015-08-05 22:05:49 -0400

    Update getExamScore.php

--- middleware/getExamScore.php
+++ middleware/getExamScore.php
@@ -11,66 +11,42 @@ else{
 
 
 $name = $user_examData["username"];
-/*
-$urlforget="https://web.njit.edu/~jap64/backend/user.php";
-$urlforget=$urlforget."?username=".$name;
-$SID = curlGet($urlforget);
-
-$SID = json_decode($SID,true);
-$user_examData["userId"] = $SID["id"];*/
 $user_examData["userId"] = getStudentIdfromUsername($name);
-
-
 
 $ListofAllStudentAnswers = sendTo("https://web.njit.edu/~sra27/getStudentExam.php",json_encode($user_examData));
 $ListofAllStudentAnswers = json_decode($ListofAllStudentAnswers,true);
 
-
-
-/*
-$userId=$exam["userId"];
-$metaData["userId"]=$userId;
-unset($exam["userId"]);
-
-$examId = $exam["examId"];
-$metaData["examId"]=$examId;
-unset($exam["examId"]);
-*/
-
-
-
+$examId=$user_examData["examId"];
 
 $exam_total = 0;
 $student_score = 0;
 
-//echo "[";
 $count=0;
 
 foreach ($ListofAllStudentAnswers as $student_question_id){
-    if($count > 0){
-      //  echo ",";
-  }
 
   $questionId=$student_question_id["questionId"];
 
   $answer=getQuestionfromID($student_question_id["questionId"]);
 
   $answer=json_decode($answer,true);
-  //  $student_answer=$ListofAllStudentAnswers[$questionId];
-  //$student_answer=$student_question_id[$questionId];
+
   $student_answer=$student_question_id;
-  //  echo $student_answer["answer"];
 
-  //    echo $student_answer["questionId"];
+  $examQuestionUrl = "https://web.njit.edu/~jap64/backend/examquestion.php?examId=".$examId."&questionId=".$questionId;
+  $examQuestion = curlGet($examQuestionUrl);
 
-  //make sure this is a valid
-    $exam_total+= $answer["points"];
+  $examQuestion = json_decode($examQuestion,true);
 
-    //     echo $answer["points"];
+  $points = $examQuestion["0"];
+
+  $points = $points["points"];
+  $exam_total += $points;
 
     if(strtolower($student_answer["answer"]) == strtolower($answer["answer"])){
 
-    $student_score += $answer["points"];
+      $student_score += $points;
+
   }
 
   $Data["questionId"] = $questionId;
@@ -79,28 +55,19 @@ foreach ($ListofAllStudentAnswers as $student_question_id){
   $Data["text"] = $answer["text"];
   $Data["format"] = $answer["format"];
   $testjson[$questionId]=json_encode($Data);
-  //  echo json_encode($Data);
-
 
   $count++;
 }
 
-//echo $student_score;
+
 $metaData["userId"]=$user_examData["userId"];
 $metaData["examId"]=$user_examData["examId"];
 $metaData["grade"] = ($student_score/$exam_total) * 100;
 $metaData=json_encode($metaData);
 $testjson["metaData"]=$metaData;
 $testjson= json_encode($testjson);
+
 echo $testjson;
-
-//$testjson= json_decode($testjson,true);
-//$testjson= json_decode($testjson["metaData"],true);
-//echo $testjson["userId"];
-
-
-
-//echo "]";
 
 
 ?>
